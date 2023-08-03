7b690402f73ddf389daeda1e39b9a4b279978c31
Anuradha Basnayake <2020cs020@stu.ucsc.cmb.ac.lk>
2023-08-03 08:37:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBg67SF8AVu0j2SXa6JmBKi5B6kf-a0u2o",
-    authDomain: "linkedin-clone-861ab.firebaseapp.com",
-    projectId: "linkedin-clone-861ab",
-    storageBucket: "linkedin-clone-861ab.appspot.com",
-    messagingSenderId: "328197573609",
-    appId: "1:328197573609:web:e3b2fa6184c0da02561978"
+    apiKey: "API KEY",
+    authDomain: "DOMAIN ADDRESS",
+    projectId: "PROJECT ID",
+    storageBucket: "STORAGE BUCKET",
+    messagingSenderId: "MESSAGING SENDER ID",
+    appId: "APP ID"
   };
 
   // const firebaseApp = firebase.initializeApp(firebaseConfig);
